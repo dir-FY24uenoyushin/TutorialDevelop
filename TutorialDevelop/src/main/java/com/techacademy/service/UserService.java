141137737f95efca1569e3c6b0c9cacaeaf6f042
dir-FY24uenoyushin <yushin.ueno.x100@dirbato.co.jp>
2024-04-24 15:15:56 +0900

    新規User登録

--- TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
+++ TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional; // 追加
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -12,9 +13,19 @@ import com.techacademy.repository.UserRepository;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
     /** 全件を検索して返す */
     public List<User> getUserList() {
         // リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
     }
+
+    // ----- 追加:ここから -----
+    /** Userの登録を行う */
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+    // ----- 追加:ここまで -----
+
 }
